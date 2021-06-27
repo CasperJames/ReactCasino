93e78729116eb86be266d88ad9d7ccfcc66cced9
Ubuntu <ubuntu@ip-172-31-44-145.us-east-2.compute.internal>
2021-06-27 15:44:50 +0000

    Modify about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,13 +6,13 @@
 	<div class="col-md-4"></div>
 	<div class="col-md-8">
 	    <div class="row">
-	        <div class="col-md-12">
+	        <div class="col-md-12"><br />
 	            <h1>Thank you for stopping by!</h1>
 		<div />
 	    </div>
 	    <div class="row">
 	        <div class="col-md-12">
-		    <p class="aboutP">This project was made utilizing Laravel 8.48.2, React JS 17.0.2, Ubuntu 20.04.2 LTS, AWS EC2 t2.Medium. It currently being developed by Caleb Hess as a learning playground for Laravel and React. </p><p class="aboutP">Playing card images inside the game are used under Attribution-NonCommercial 4.0 International by http://pngimg.com located at <a href="http://pngimg.com/license">http://pngimg.com/license</a>.</p><p class="aboutP"> Background image used under Shutterstock Standard Licensei located at <a href="https://www.shutterstock.com/license">https://www.shutterstock.com/license</a> 1.1.</p><p class="aboutP">Blackjack logic forked from Jeff's project on CodePen here <a href="https://codepen.io/jeffleu/pen/MbVGmM">https://codepen.io/jeffleu/pen/MbVGmM</a>under the Public Open Source License found here <a href="https://blog.codepen.io/documentation/licensing/">https://blog.codepen.io/documentation/licensing/</a>.</p>
+		    <p class="aboutP">This project was made utilizing Laravel 8.48.2, React JS 17.0.2, Ubuntu 20.04.2 LTS, AWS EC2 t2.Medium. It currently being developed by Caleb Hess as a learning playground for Laravel and React. </p><br /><p class="aboutP">Playing card images inside the game are used under Attribution-NonCommercial 4.0 International by http://pngimg.com located at <a href="http://pngimg.com/license">http://pngimg.com/license</a>.</p><br /><p class="aboutP"> Background image used under Shutterstock Standard License located at <a href="https://www.shutterstock.com/license">https://www.shutterstock.com/license</a> 1.1.</p><br /><p class="aboutP">Blackjack logic forked from Jeff's project on CodePen here <a href="https://codepen.io/jeffleu/pen/MbVGmM">https://codepen.io/jeffleu/pen/MbVGmM</a>under the Public Open Source License found here <a href="https://blog.codepen.io/documentation/licensing/">https://blog.codepen.io/documentation/licensing/</a>.</p><br /><p class="aboutP">Code on GitHub here <a href="https://github.com/CasperJames/ReactCasino">https://github.com/CasperJames/ReactCasino</a></p>
 		</div>
 	    </div>
         </div>
